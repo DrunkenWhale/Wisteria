0adab4b26a2baddd18386a34607c48fb57df17a5
Pigeon377 <892197240@qq.com>
2022-06-15 23:40:20 +0800

    sync: v0.0.3 Dandelion

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"Gloxinia/ds"
 	"fmt"
+	"github.com/Pigeon377/Dandelion/lsm"
 )
 
 const sep = 2
 
 func main() {
 	fmt.Println(test(ds.NewWisteriaInt(14)))
+	lsm.NewLSM().Put(114514, []byte("s"))
 }
 
 func test(w ds.Wisteria) bool {
