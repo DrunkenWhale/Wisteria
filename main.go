6fcf9be7e2244212a5b1dbd348dc49823a73c0ba
Pigeon377 <892197240@qq.com>
2022-06-17 23:45:51 +0800

    Remove useless file

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"Wisteria/ds"
-	"fmt"
 	"github.com/Pigeon377/Dandelion/lsm"
 )
 
 const sep = 2
 
 func main() {
-	fmt.Println(test(ds.NewWisteriaInt(14)))
 	lsm.NewLSM().Put(114514, []byte("s"))
 	lsm.NewLSM().ClearMemory()
 	lsm.NewLSM().ClearFilter()
-}
-
-func test(w ds.Wisteria) bool {
-	_, ok := w.(*ds.WisteriaInt)
-	return ok
 }
